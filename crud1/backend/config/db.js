29dab7372719611210477a63ff586f1b5daf8e3b
Matheus Gomes <matheus.recin@outlook.com>
2025-08-14 00:12:28 -0300

    config de ambiente e gitignore

--- crud1/backend/config/db.js
+++ crud1/backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/crud1");
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("Conectando no MongoDB...");
     } catch (err) {
         console.error("Erro ao conectar no MongoDB", err.message)
--- crud1/backend/server.js
+++ crud1/backend/server.js
@@ -2,16 +2,19 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import itemRotas from "./routes/itemRotas.js";
+import dotenv from "dotenv"
 
+dotenv.config();
 
 const app = express();
 connectDB();
 
-app.use(cors({
-    origin: ["http://localhost:5001", "http://localhost:3001"],
-    methods: ["GET", "POST", "DELETE"],
-    allowedHeaders: ["Content-Type"]
-}));
+/*app.use(cors({
+    origin: ["http://localhost:5001", "http://localhost:3001"],
+    methods: ["GET", "POST", "DELETE"],
+    allowedHeaders: ["Content-Type"]
+}));*/
+//app.use(); // habilita todas urls...
 app.use(express.json());
 
 app.use("/api/itens", itemRotas)
