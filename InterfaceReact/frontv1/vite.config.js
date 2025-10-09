2657597ff5741b819850ccef75c87cd8aa22dc59
Matheus Gomes <matheus.recin@outlook.com>
2025-10-09 17:33:55 -0300

    feat: configuração de ambiente servidor local intranet.testes

--- InterfaceReact/frontv1/vite.config.js
+++ InterfaceReact/frontv1/vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+    server: {
+      allowedHosts: ['intranet.testes', 'localhost']
+    },
   plugins: [
       react(),
       tailwindcss(),
