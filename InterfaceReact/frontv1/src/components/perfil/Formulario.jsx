61d13a6e91a61cc987ad48f83f3176a9a72b8dac
Matheus Gomes <matheus.recin@outlook.com>
2025-11-27 21:11:37 -0300

    feat: iniciado formulario perfil vendedor

--- InterfaceReact/frontv1/src/components/perfil/Formulario.jsx
+++ InterfaceReact/frontv1/src/components/perfil/Formulario.jsx
@@ -1,9 +1,19 @@
-export const Formulario = ({}) => {
+import { InputModal } from "../clientes/ClienteModal"
+
+export const FormularioPerfil = ({vendedor}) => {
 
     return (
-        <div>
-            <form>
+        <div className="mt-6 p-10">
+            <form className={"flex flex-col gap-2 w-full max-w-md"}>
                 <label htmlFor="nomeVendedor">Nome</label>
+                <InputModal type="text" name="nomeVendedor" id="nomeVendedor" defaultValue={vendedor?.nome || ""}/>
+                
+                <label htmlFor="email">Email</label>
+                <InputModal type="text" name="email" id="email" defaultValue={vendedor?.email || ""}/>
+                
+                <label htmlFor="ramal">Ramal</label>
+                <InputModal type="text" name="ramal" id="ramal" defaultValue={vendedor?.ramal || ""}/>
+                
             </form>
         </div>
     )
--- InterfaceReact/frontv1/src/components/perfil/perfil.jsx
+++ InterfaceReact/frontv1/src/components/perfil/perfil.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { Cabecalho } from '../ui/heading';
+import { BlueButton, GreenButton } from '../ui/buttons';
+import { FormularioPerfil } from './Formulario';
 
 const Perfil = () => {
     return (
-        <Cabecalho text="Perfil Vendedor"/>
-        
+        <div className='p-6'>
+            <Cabecalho text="Perfil Vendedor">
+                <div className='flex gap-4 mt-6'>
+                    <BlueButton onClick={() => console.log("Editando.")}> 
+                        Editar Cadastro
+                    </BlueButton>
+                    <GreenButton onClick={() => console.log("Adicionar novo vendedor.")}>
+                        Adicionar Novo
+                    </GreenButton>
+                </div>
+            </Cabecalho>
+            <div>
+                <FormularioPerfil/>
+            </div>
+        </div>        
     );
 };
 
